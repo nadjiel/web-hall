3cda4db5896da396807b7af2a8fe6164d96fc46d
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-07-26 23:15:59 -0300

    :sparkles: feat: created initial circles

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { tsParticles } from "@tsparticles/engine";
 import { loadBasic } from "@tsparticles/basic";
+import { loadEmittersPlugin } from "@tsparticles/plugin-emitters";
+
 import { options } from "./options";
 
 import "./style.css";
@@ -7,6 +9,7 @@ import "./style.css";
 const div = document.querySelector("#tsparticles")! as HTMLElement;
 
 await loadBasic(tsParticles);
+await loadEmittersPlugin(tsParticles);
 
 tsParticles.load({
   element: div,
